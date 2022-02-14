b1e0188e648c001211057997e8b60f14a3a41b16
erichanse <98995998+erichanse@users.noreply.github.com>
2022-02-14 09:10:36 -0800

    Add files via upload

--- Kong_RNASeq/Slurm_STAR.sh
+++ Kong_RNASeq/Slurm_STAR.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-#SBATCH --job-name=erich1  
+#SBATCH --job-name=erich  
 #SBATCH -p free              
 #SBATCH --nodes=1            
-#SBATCH --ntasks=16           
-#SBATCH --mem-per-cpu=24G     
+#SBATCH --ntasks=8           
+#SBATCH --mem-per-cpu=30G     
 #SBATCH --error=%x.%A.err   
-#SBATCH --out=%x.%A.out      
+#SBATCH --out=%x.%A.out  
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ehanse
 
 hn=`hostname`
 echo "Running job on host $hn"
 module purge
 module load star
-STAR --runThreadN 1 \
+STAR --runThreadN 8 \
      --runMode genomeGenerate \
-     --genomeDir /data/homezvol1/ehanse/Kong_RNASeq/data/Genome_Directory \
+     --genomeDir /pub/ehanse/Genome_Directory \
      --genomeFastaFiles /data/homezvol1/ehanse/Kong_RNASeq/data/mm39/GRCm39.genome.fa \
      --sjdbGTFfile /data/homezvol1/ehanse/Kong_RNASeq/data/GTF_Dir/gencode.vM28.annotation.gtf \
      --sjdbOverhang 99
-

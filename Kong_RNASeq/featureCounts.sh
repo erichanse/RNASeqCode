4715727126d5a7f5fa4402a4ac06520a50c947af
erichanse <98995998+erichanse@users.noreply.github.com>
2023-03-08 09:59:06 -0800

    Add files via upload

--- Kong_RNASeq/featureCounts.sh
+++ Kong_RNASeq/featureCounts.sh
@@ -1,9 +1,13 @@
 ## Generate a file with the annotated genes mapped and counted
 ## install subread and feature counts
 conda install -c bioconda subread
+## open featurecounts through subread
+conda create -c bioconda -n featurecounts subread
+## activate the featurecounts channel
+conda activate featurecounts
 
 ## run feature counts
-featureCounts -p -a gencode.vM28.annotation.gtf -o count2.out -T 4 *.bam
+featureCounts -p -a gencode.vM28.annotation.gtf -o count.out -T 4 *.bam
 ## p is paired end
 ## a is the annotation file
 ## o is the output
